3edc7e58ab9980065102aeb4db3d2a4367aad930
Rabindra <rabee.tmg123@gmail.com>
2023-06-01 12:19:36 +0545

    New configuration

--- Attendees/settings.py
+++ Attendees/settings.py
@@ -85,16 +85,16 @@ WSGI_APPLICATION = 'Attendees.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #  'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'Attendees',
-    #     'USER': 'sysadmin',
-    #     'PASSWORD':'sysadmin123',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Attendees',
+        'USER': 'sysadmin',
+        'PASSWORD':'sysadmin123',
+    }
 }
 
 
